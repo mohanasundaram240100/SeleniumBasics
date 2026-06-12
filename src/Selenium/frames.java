7460b6dd48b8cedeea2cb5a4df87b70de60a9c69
mohanasundaramp <mohanasundaramp@localhost>
2026-06-12 22:02:57 +0530

    DC

---
+++ src/Selenium/frames.java
@@ -0,0 +1,51 @@
+package Selenium;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Set;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class frames {
+	
+	WebDriver driver;
+	
+	@BeforeClass
+	void setUp() 
+	{
+	driver= new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+	driver.get("https://www.iconplc.com");
+	}
+	
+	@Test
+	void test() 
+	{
+		Set<String> windowHandles = driver.getWindowHandles();
+		System.out.println(windowHandles);
+		
+		ArrayList<String> windowList = new ArrayList<String>(windowHandles);
+		String parentWindow = windowList.get(0);
+		String childWindow = windowList.get(1);
+		
+		driver.switchTo().window(childWindow);
+		System.out.println("Child Window Title: " + driver.getTitle());
+
+		
+	}
+	
+	
+	
+	@AfterClass 
+	void tearDown() 
+	{
+		driver.quit();
+	}
+	
+
+}
