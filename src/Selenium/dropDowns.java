42b30c95f349d84d1deac73d180747cca60421b3
mohanasundaramp <mohanasundaramp@localhost>
2026-06-19 12:32:31 +0530

    DC

--- src/Selenium/dropDowns.java
+++ src/Selenium/dropDowns.java
@@ -1,6 +1,37 @@
 
 package Selenium;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
 public class dropDowns {
+	
+	public static void main (String [] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+		
+		WebElement day = driver.findElement(By.id("day"));
+		Select select = new Select(day);
+		select.selectByIndex(5);
+		
+		WebElement month = driver.findElement(By.id("month"));
+		Select select1 = new Select(month);
+		select1.selectByVisibleText("May");
+		
+		WebElement year = driver.findElement(By.id("year"));
+		Select select2 = new Select(year);
+		select2.selectByValue("1990");
+		
+		driver.quit();
+		
+	}
 
 }
