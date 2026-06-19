211222ef3521e0e27c27dc750264138159ac880a
mohanasundaramp <mohanasundaramp@localhost>
2026-06-19 13:05:25 +0530

    DC

--- src/Selenium/keyBoardActions.java
+++ src/Selenium/keyBoardActions.java
@@ -2,8 +2,10 @@ package Selenium;
 
 import java.time.Duration;
 
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class keyBoardActions {
 	
@@ -14,6 +16,11 @@ public class keyBoardActions {
 		driver.get("https://www.facebook.com/");
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
 		
+		Actions action = new Actions(driver);
+		action.keyDown(Keys.CONTROL).sendKeys("a").keyUp(Keys.CONTROL).build().perform();
+		action.keyDown(Keys.CONTROL).sendKeys("c").keyUp(Keys.CONTROL).build().perform();
+		
+		action.keyDown(Keys.TAB).keyUp(Keys.TAB).build().perform();
 		
 		
 		
